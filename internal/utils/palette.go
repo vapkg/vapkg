72d4d7792158d32a3d86e963ea5e972346aae5cb
rejchev <hendlerq@yandex.ru>
2025-06-20 03:52:46 +0500

    palette: added non-prefixed printers

--- internal/utils/palette.go
+++ internal/utils/palette.go
@@ -17,16 +17,40 @@ func VaPrefix() string {
 	return vaPrefix
 }
 
+func VaSprint(args ...any) string {
+	return palette.Use(fmt.Sprint(args...))
+}
+
+func VaSprintf(format string, args ...any) string {
+	return palette.Use(fmt.Sprintf(format, args...))
+}
+
+func VaSprintln(args ...any) string {
+	return palette.Use(fmt.Sprintln(args...))
+}
+
+func VaPrintWithPrefix(args ...any) (n int, err error) {
+	return fmt.Fprint(os.Stdout, VaSprint(append([]any{VaPrefix(), " "}, args...)...))
+}
+
 func VaPrint(args ...any) (n int, err error) {
-	return fmt.Fprint(os.Stdout, palette.Use(VaPrefix()+" "+fmt.Sprint(args...)))
+	return fmt.Fprint(os.Stdout, VaSprint(args...))
 }
 
 func VaPrintln(args ...any) (n int, err error) {
-	return fmt.Fprintln(os.Stdout, palette.Use(VaPrefix()+" "+fmt.Sprintln(args...)))
+	return fmt.Fprintln(os.Stdout, VaSprint(args...))
+}
+
+func VaPrintlnWithPrefix(args ...any) (n int, err error) {
+	return fmt.Fprintln(os.Stdout, VaSprint(append([]any{VaPrefix(), " "}, args...)...))
 }
 
 func VaPrintf(format string, args ...any) (n int, err error) {
-	return fmt.Fprintf(os.Stdout, palette.Use(VaPrefix()+" "+fmt.Sprintf(format, args...)))
+	return fmt.Fprintf(os.Stdout, VaSprintf(format, args...))
+}
+
+func VaPrintfWithPrefix(format string, args ...any) (n int, err error) {
+	return fmt.Fprintf(os.Stdout, VaSprintf("%s "+format, append([]any{VaPrefix()}, args...)...))
 }
 
 func InitVaPalette() {
